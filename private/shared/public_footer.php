aabbe8f728a3bcde23840bbfc28430de50f0067b
Bartek <bartosz.charleza@gmail.com>
2024-06-27 15:22:06 +0200

    Hello my CMS

---
+++ private/shared/public_footer.php
@@ -0,0 +1,11 @@
+  <footer>
+    <p>Copyright <?php echo date('Y'); ?>, My CMS </p>
+  </footer>
+
+  </body>
+
+  </html>
+
+  <?php
+  db_disconnect($db);
+  ?>
---
+++ private/shared/staff_footer.php
@@ -0,0 +1,11 @@
+<footer>
+  &copy; <?php echo date('Y'); ?> My CMS
+</footer>
+
+</body>
+
+</html>
+
+<?php
+db_disconnect($db);
+?>
